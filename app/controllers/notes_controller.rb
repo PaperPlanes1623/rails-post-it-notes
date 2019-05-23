1fb63112fa3355f6e09f365b00a1ee2da74ef65c
Dylan Taft <dylbronjames@gmail.com>
2019-05-22 19:14:44 -0600

    updated crud actions for controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,23 @@
 class NotesController < ApplicationController
   def index
+    @notes = Note.all
   end
 
   def show
+    @note = Note.find(params[:id])
   end
 
   def edit
+    @note = Note.find(params[:id])
   end
 
   def new
+    @note = Note.new
   end
+
+  def destroy 
+    Note.find(params[:id]).destroy 
+    # redirect_to pages_path --->edit this line
+  end
+
 end
