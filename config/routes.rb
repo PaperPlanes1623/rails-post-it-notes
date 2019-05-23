300ffbdef41df58825eb2f130034155e8f0a78ef
Dylan Taft <dylbronjames@gmail.com>
2019-05-22 19:05:15 -0600

    init project

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'notes/index'
+  get 'notes/show'
+  get 'notes/edit'
+  get 'notes/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
